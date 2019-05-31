64f16ab0b58a87faebd70116cdd1909d84dadf7b
roastduckcd <roart_duck@163.com>
2019-05-31 10:34:28 +0800

    base primitive - use stock shader - identity

--- BasePrimitive/BasePrimitive/main.cpp
+++ BasePrimitive/BasePrimitive/main.cpp
@@ -10,6 +10,9 @@
 #include <GL/glut.h>
 #endif
 
+GLBatch pointBatch;
+GLShaderManager shaderManager;
+
 void prepareToRender(int argc, char *argv[]) {
     // 设置工作空间，默认可执行文件目录
     gltSetWorkingDirectory(argv[0]);
@@ -23,9 +26,18 @@ void prepareToRender(int argc, char *argv[]) {
     if (GLEW_OK != state) {
         printf("api 不支持！\n");
     }
+    shaderManager.InitializeStockShaders();
 }
 
 void render(void) {
+
+    GLfloat rgbaColor[] = {1,0,0,1};
+    // 指定着色器
+    shaderManager.UseStockShader(GLT_SHADER_IDENTITY, rgbaColor);
+    // 开始绘制
+    pointBatch.Draw();
+    // 交换缓冲区
+    glutSwapBuffers();
 }
 
 void registerCallback(void) {
@@ -43,6 +55,8 @@ void simplistPipeline(void) {
     glPointSize(5);
         // 设定图元样式为点
     glBegin(GL_POINTS);
+        // 设定图元样式为环
+//    glBegin(GL_LINE_LOOP);
 
     glVertex3f(3, 0, 0);
     glVertex3f(0, 3, 0);
@@ -55,6 +69,22 @@ void simplistPipeline(void) {
         //    glutSwapBuffers();
 }
 
+void setupVertexData() {
+    GLfloat vertexes[] = {
+        0.5, 0, 0,
+        0, 0.5, 0,
+        0, 0, 0.5,
+    };
+    glPointSize(5);
+        // 指定渲染的图元方式和顶点数量（x,y,z）为一个顶点
+//    pointBatch.Begin(GL_POINTS, 3);
+    pointBatch.Begin(GL_LINE_LOOP, 3);
+        // 复制顶点数据，处理成向量顶点的形式
+    pointBatch.CopyVertexData3f(vertexes);
+        // 结束处理，和begin成套
+    pointBatch.End();
+}
+
 
 int main(int argc, char *argv[]) {
 //    static_assert(false, "请修改 PublicOpenGLHeaderPath 为你的工程目录（include上一级目录）: $(SRCROOT)/工程名");
@@ -63,7 +93,8 @@ int main(int argc, char *argv[]) {
 
     registerCallback();
 
-    simplistPipeline();
+//    simplistPipeline();
+    setupVertexData();
 
         // 开启监听循环
     glutMainLoop();
